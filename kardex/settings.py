bd194cfadca4ad522291f2a1d6d2381f69ca0d65
Alejandra <alejandraguasase7@gmail.com>
2026-04-01 17:11:19 -0400

    actualizado

--- kardex/settings.py
+++ kardex/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "histolink",      # Nombre de tu base de datos
         "USER": "postgres",       # Tu usuario de postgres
-        "PASSWORD": "12345678",   # Tu contraseña
+        "PASSWORD": "alejandra",   # Tu contraseña
         "HOST": "localhost",
         "PORT": "5432",
     }
