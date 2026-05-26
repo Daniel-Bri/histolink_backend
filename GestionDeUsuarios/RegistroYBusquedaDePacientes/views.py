495c5eec50305329a78a23e1ca0dd11e44b73086
Alejandra <alejandraguasase7@gmail.com>
2026-05-25 20:15:11 -0400

    correcciones de T017

--- GestionDeUsuarios/RegistroYBusquedaDePacientes/views.py
+++ GestionDeUsuarios/RegistroYBusquedaDePacientes/views.py
@@ -3,6 +3,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import PageNumberPagination
 from .models import Paciente
 from .serializers import PacienteSerializer
+from AtencionClinica.ConsultaMedicaSOAP.models import Consulta
+from AtencionClinica.RegistroDeTriaje.models import Triaje
 from GestionDeUsuarios.LoginYAutenticacion.permissions import (
     EsMedicoEnfermeroOAdmin,
     EsAdminODirector,
@@ -22,7 +24,33 @@ class PacienteViewSet(viewsets.ModelViewSet):
     pagination_class = PacientePagination
 
     def get_queryset(self):
-        return Paciente.objects.all().order_by('-creado_en')
+        qs = Paciente.objects.all().order_by('-creado_en')
+        mis_pacientes = (self.request.query_params.get('mis_pacientes') or '').lower() in ('1', 'true', 'si', 'yes')
+        if not mis_pacientes:
+            return qs
+
+        user = self.request.user
+        if not user.is_authenticated:
+            return qs.none()
+        if user.is_superuser or user.groups.filter(name__in=['Director', 'Administrativo', 'Auditor']).exists():
+            return qs
+
+        if user.groups.filter(name='Médico').exists():
+            paciente_ids = set(
+                Consulta.objects.filter(medico=user).values_list('ficha__paciente_id', flat=True)
+            )
+            paciente_ids.update(
+                Paciente.objects.filter(creado_por=user).values_list('id', flat=True)
+            )
+            return qs.filter(id__in=paciente_ids)
+
+        if user.groups.filter(name='Enfermera').exists():
+            paciente_ids = set(
+                Triaje.objects.filter(enfermera=user).values_list('ficha__paciente_id', flat=True)
+            )
+            return qs.filter(id__in=paciente_ids)
+
+        return qs.none()
 
     def get_permissions(self):
         if self.action in ['list', 'retrieve']:
